9b2211c4280e3369a524bfb0881edc26db0b04a2
RODHOLFO_EBAC <ro.garo87@gmail.com>
2023-08-02 04:37:22 -0300

    setp do Projeto

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+/*
+
+1ª
+//Criar tarefa teste, para ver se o Gulp Fnciona corretamente
+function testeGulp(cb){
+    console.log("Ola Mundo");
+    // CallBack, deve ser passado como argumento da tarefa
+    cb();
+}
+exports.default = testeGulp;
+
+// node .\node_modules\gulp\bin\gulp.js, Para não ter que usar este comando, colocamos o gulp no script do package.json e assim o gulp vai rodar normalmente
+
+*/
+
+// 2ª Configurando o sass no gulp - Exportar plugins Instalados
+const gulp =require('gulp');
+const sass =require ('gulp-sass') (require('sass'));
+const imagemin = require('gulp-imagemin');
+
+// Recuperar os arquivos
+function styles(){
+    return gulp.src('./src/styles/*.scss')
+    .pipe(sass({outputStyle:'compressed'}))//Comprime os arquivos
+    .pipe(gulp.dest('./dist/css'));
+}
+
+// Reeduzir imagens
+function images(){
+    return gulp.src('./src/images/**/*')
+    .pipe(imagemin())//É uma função
+    .pipe(gulp.dest('./dist/images'));
+}
+
+exports.default = gulp.parallel(styles, images);
+// Criar função para observar o Gulp
+exports.watch = ('./src/styles/*.scss', gulp.parallel(styles))
---
+++ passos.md
@@ -0,0 +1,22 @@
+1ª Montar estrutura Basica
+
+ - Instalar node
+ - Instalar GULP
+ - Instalar SASS
+ - Configurar Gulp
+    - Testar GULP
+    - Compilar SASS
+    - Comprimir CSS
+    - Instalar Wacth
+ - Criar pasta dist
+ - Arquivo .gitignore
+
+2ª
+- Comprimir imgens no GULP
+    - npm install --save-dev gulp-imagemin@7.1.0 (Versão 7.1.0)
+    - COnfigurar no GULP file
+- Trabalhar no sass
+    - Criar Variaveis de cor
+    - Criar mixin
+    - Criar classe para o texto
+- Construir Hero
